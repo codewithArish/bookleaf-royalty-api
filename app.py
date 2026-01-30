02ba61acebb547a71c4074791b940779ba7c1cbf
Mohammad Arish <mohammadarish2510@gmail.com>
2026-01-30 12:46:32 +0530

    Update app run configuration to use environment port

--- app.py
+++ app.py
@@ -178,4 +178,6 @@ def get_author_withdrawals(author_id):
 # ------------------ RUN APP ------------------
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
